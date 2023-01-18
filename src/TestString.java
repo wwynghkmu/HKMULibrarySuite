5ef6a1366ed3dc8df86a57911df86e2c201dbab7
wwynghkmu <wwyng@hkmu.edu.hk>
2023-01-18 12:36:38 +0800

    20230118

--- src/TestString.java
+++ src/TestString.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,6 +16,24 @@ public class TestString {
 		String str = "TOLOGY–CRYPTO2019SPRINGER";
 		str = StringHandling.trimSpecialChars(str);
 		System.out.println(str);
+		
+	    try {
+	    	String[] wordsArray;
+	        File myObj = new File("d:\\Z108_course_202301017.csv");
+	        Scanner myReader = new Scanner(myObj);
+	        while (myReader.hasNextLine()) {
+	          String data = myReader.nextLine();
+	          wordsArray = data.split("\t");
+	          System.out.println(wordsArray[0]);
+	          System.out.println(wordsArray[1]);
+	          System.out.println(wordsArray[2]);
+	          System.out.println(data);
+	        }
+	        myReader.close();
+	      } catch (FileNotFoundException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	      }
 
 	}
 
