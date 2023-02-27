1d83263abcf18282ac56289ff2a09bb5d9902320
wwynghkmu <wwyng@hkmu.edu.hk>
2023-02-27 17:48:14 +0800

    20230227

--- src/TestACQConfig.java
+++ src/TestACQConfig.java
@@ -1,13 +1,11 @@
 
-import hk.edu.hkmu.lib.acq.*;
+import hk.edu.hkmu.lib.cir.*;
 
 public class TestACQConfig {
 
 	public static void main(String[] argvs) {
-		hk.edu.hkmu.lib.acq.Config.init();	
-		System.out.println(hk.edu.hkmu.lib.Config.DB_URL);
-		System.out.println(hk.edu.hkmu.lib.Config.USER);
-		System.out.println(hk.edu.hkmu.lib.Config.PASS);
+		Course c = new Course();	
+
 	}
 
 }
