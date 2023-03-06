78c630fe85412f7a63fcb68c6e22bdad7d6ffbc0
wwynghkmu <wwyng@hkmu.edu.hk>
2023-03-06 13:57:41 +0800

    20230306-2

--- src/TestCourse.java
+++ src/TestCourse.java
@@ -4,7 +4,13 @@ import hk.edu.hkmu.lib.cir.*;
 public class TestCourse {
 
 	public static void main(String[] argvs) {
-		Course c = new Course("https://api-ap.hosted.exlibrisgroup.com/almaws/v1/courses?apikey=l8xxeb2a38bf0cb840e59f4ecf7a8f1c01f0&status=ACTIVE&order_by=code&limit=100&offset=0");
+		CourseTest c = new CourseTest(
+				"https://api-ap.hosted.exlibrisgroup.com/almaws/v1/courses?apikey=l8xxeb2a38bf0cb840e59f4ecf7a8f1c01f0&status=ACTIVE&order_by=code&limit=100&offset=0",
+				"d:\\");
+		for (int i = 0; i < c.courseCodeArry.size(); i++) {
+			System.out.println(c.courseCodeArry.get(i).toString());
+			System.out.println(c.courseNameArry.get(i).toString());
+		}
 	}
 
 }
--- src/hk/edu/hkmu/lib/cir/CourseTest.java
+++ src/hk/edu/hkmu/lib/cir/CourseTest.java
@@ -1,19 +1,20 @@
 package hk.edu.hkmu.lib.cir;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
+import java.util.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.lang3.time.DateUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-public class Course {
+public class CourseTest {
+	HashMap<String, ArrayList> CourseHashMap = new HashMap<String, ArrayList>();
 	public ArrayList courseCodeArry = new ArrayList();
 	public ArrayList courseNameArry = new ArrayList();
 	public ArrayList statusArry = new ArrayList();
@@ -22,13 +23,17 @@ public class Course {
 	public int index = 0;
 	public int offset = 0;
 	private String urlStr = "";
+	String tempDir = "";
+	URLConnection urlcon;
 
-	public Course() {
+	public CourseTest(String tempDir) {
+		this.tempDir = tempDir;
 		readCourses();
 	}
 
-	public Course(String urlStr) {
+	public CourseTest(String urlStr, String tempDir) {
 		this.urlStr = urlStr;
+		this.tempDir = tempDir;
 		readCourses();
 	}
 
@@ -37,7 +42,51 @@ public class Course {
 		String urlStr = "https://api-ap.hosted.exlibrisgroup.com/almaws/v1/courses?apikey=l8xx41bf077192274811b545c60ba48df465&status=ACTIVE&order_by=code&limit=100&offset=0";
 		if (this.urlStr.equals(""))
 			this.urlStr = urlStr;
-		readNextCourses();
+		try {
+			File f = new File(tempDir + "Courses.ser");
+			if (f.exists()) {
+
+				long lmodify = f.lastModified();
+				Date lmdate = new Date(lmodify);
+				DateUtils du = new DateUtils();
+
+				Date today = new Date();
+				if (du.isSameDay(lmdate, today)) {
+					FileInputStream fileIn = new FileInputStream(tempDir + "Courses.ser");
+					ObjectInputStream in = new ObjectInputStream(fileIn);
+					CourseHashMap = (HashMap) in.readObject();
+					in.close();
+					fileIn.close();
+					courseCodeArry = CourseHashMap.get("courseCodeArry");
+					courseNameArry = CourseHashMap.get("courseNameArry");
+					statusArry = CourseHashMap.get("statusArry");
+					departmentArry = CourseHashMap.get("departmentArry");
+				} else {
+					readNextCourses();
+					CourseHashMap.put("courseCodeArry", courseCodeArry);
+					CourseHashMap.put("courseNameArry", courseNameArry);
+					CourseHashMap.put("statusArry", statusArry);
+					CourseHashMap.put("departmentArry", departmentArry);
+					FileOutputStream fileOut = new FileOutputStream(tempDir + "Courses.ser");
+					ObjectOutputStream out = new ObjectOutputStream(fileOut);
+					out.writeObject(CourseHashMap);
+
+				}
+
+			} else {
+				readNextCourses();
+				CourseHashMap.put("courseCodeArry", courseCodeArry);
+				CourseHashMap.put("courseNameArry", courseNameArry);
+				CourseHashMap.put("statusArry", statusArry);
+				CourseHashMap.put("departmentArry", departmentArry);
+				FileOutputStream fileOut = new FileOutputStream(tempDir + "Courses.ser");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(CourseHashMap);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("TOTL: " + totalRecord);
 
 	}
@@ -49,7 +98,8 @@ public class Course {
 		offset += 100;
 		try {
 			URL url = new URL(urlStr);
-			URLConnection urlcon = url.openConnection();
+			System.out.println(urlStr);
+			urlcon = url.openConnection();
 
 			urlcon.setConnectTimeout(3000);
 			BufferedReader buffread = new BufferedReader(new InputStreamReader(urlcon.getInputStream(), "UTF-8"));
@@ -57,7 +107,7 @@ public class Course {
 			while ((inputLine = buffread.readLine()) != null)
 				outstr += inputLine;
 			buffread.close();
-			
+
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			InputSource is = new InputSource(new StringReader(outstr));
@@ -71,33 +121,29 @@ public class Course {
 				if (nlist2.item(i).getFirstChild() != null)
 					courseCode = nlist2.item(i).getFirstChild().getNodeValue();
 				courseCodeArry.add(i, courseCode);
-				System.out.print(courseCodeArry.get(i));
 
 				nlist2 = doc.getElementsByTagName("name");
 				String courseName = "";
 				if (nlist2.item(i).getFirstChild() != null)
 					courseName = nlist2.item(i).getFirstChild().getNodeValue();
 				courseNameArry.add(i, courseName);
-				System.out.print(courseName);
 
 				nlist2 = doc.getElementsByTagName("status");
 				String status = "";
 				if (nlist2.item(i).getFirstChild() != null)
 					status = nlist2.item(i).getFirstChild().getNodeValue();
 				statusArry.add(i, status);
-				System.out.print(status);
 
 				nlist2 = doc.getElementsByTagName("academic_department");
 				String department = "";
 				if (nlist2.item(i).getFirstChild() != null)
 					department = nlist2.item(i).getFirstChild().getNodeValue();
 				departmentArry.add(i, department);
-				System.out.println(department);
+
 			}
 			if (totalRecord % 100 == 0) {
 				readNextCourses();
 			}
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
